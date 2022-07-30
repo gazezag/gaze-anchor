9cb724bbed861be6f4fbeeefdc1669923766156d
Ethan Teng <1345118870@qq.com>
2022-07-30 12:27:59 +0800

    removed personal info comment

--- packages/index.ts
+++ packages/index.ts
@@ -1,10 +1,3 @@
-/**
- * @Author: Ethan Teng
- * @Date: 2022-07-29 11:05:53
- * @LastEditTime: 2022-07-30 11:04:26
- * @Description:
- */
-
 export const add = (x: number, y: number): number => {
   return x + y;
 };
