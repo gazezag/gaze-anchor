a167d1c676482cdd9da32f7b0634b7df26d9bbc4
Ethan Teng <1345118870@qq.com>
2022-07-30 15:49:29 +0800

    refactored interfaces with generics

--- packages/types/envInfo.ts
+++ packages/types/envInfo.ts
@@ -1,11 +1,27 @@
+export enum OSType {
+  Windows,
+  MacOS,
+  Linux,
+  Unknown
+}
+
+export enum BrowserType {
+  Chrome,
+  Safari,
+  Edge,
+  IE,
+  Firefox,
+  Opera,
+  Unknown
+}
+
+export interface MetaInfo<T> {
+  type: T;
+  version: string;
+}
+
 export interface EnvInfo {
   url: string;
-  os: {
-    type: 'Windows' | 'Mac OS' | 'Linux';
-    version: string;
-  };
-  browser: {
-    type: string;
-    version: string;
-  };
+  os: MetaInfo<OSType>;
+  browser: MetaInfo<BrowserType>;
 }
