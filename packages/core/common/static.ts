a0938b8bc55930163c536a9393656bf076fd1baa
Ethan Teng <1345118870@qq.com>
2022-07-31 17:09:55 +0800

    added some public static datas

---
+++ packages/core/common/static.ts
@@ -0,0 +1,13 @@
+// store some static datas
+export const EntryNames = {
+  FP: 'first-paint',
+  FCP: 'first-contentful-paint',
+  LCP: 'largest-contentful-paint'
+};
+
+export const EntryTypes = {
+  paint: 'paint',
+  navigation: 'navigation',
+
+  LCP: 'largest-contentful-paint'
+};
