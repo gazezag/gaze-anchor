b429d92b3818b1424c72da99b44cc5754a94e70a
Ethan Teng <1345118870@qq.com>
2022-08-01 11:31:24 +0800

    added a data cache to store the performance informations

---
+++ packages/core/common/store.ts
@@ -0,0 +1,34 @@
+import { PerformanceInfo, PerformanceInfoObj } from 'types/performanceIndex';
+import { PerformanceInfoType } from './static';
+
+export class Store {
+  private status: Map<PerformanceInfoType, PerformanceInfo>;
+
+  constructor() {
+    this.status = new Map();
+  }
+
+  has(name: PerformanceInfoType): boolean {
+    return this.status.has(name);
+  }
+
+  set(name: PerformanceInfoType, value: PerformanceInfo) {
+    this.status.set(name, value);
+  }
+
+  get(name: PerformanceInfoType): PerformanceInfo | null {
+    return this.has(name) ? (this.status.get(name) as PerformanceInfo) : null;
+  }
+
+  clear() {
+    this.status.clear();
+  }
+
+  getAll(): PerformanceInfoObj {
+    return Array.from(this.status).reduce((o, [k, v]) => {
+      // use reflect because it panics with o[k] = v
+      Reflect.set(o, k, v);
+      return o;
+    }, {});
+  }
+}
