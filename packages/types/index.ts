17b6b209e95c2e4a3c756c4fc43bbd26eb8d6251
Ethan Teng <1345118870@qq.com>
2022-08-01 11:22:32 +0800

    updated exported data

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,4 +1,4 @@
 export * from './apiPerformanceInfo';
-export * from './envInfo';
+export * from './deviceEnvInfo';
 export * from './errorInfo';
-export * from './requestData';
+export * from './uploader';
