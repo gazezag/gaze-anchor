d1bde259bf9a88fab22884977309f0b880623962
Ethan Teng <1345118870@qq.com>
2022-08-02 17:35:49 +0800

    added upload.ts and removed upload module

--- packages/core/common/upload.ts
+++ packages/core/common/upload.ts
@@ -1,5 +1,50 @@
 import { UploadHandler } from 'types/uploader';
 
+const imgRequest = (url: string, data: any) => {
+  if (!url || !data) return;
+
+  const img = new Image();
+
+  img.onload = () => {
+    console.log('loaded...');
+  };
+
+  img.onerror = () => {
+    console.log('error....');
+  };
+
+  img.src = `${url}${url.indexOf('?') < 0 ? '?' : '&'}${encodeURIComponent(JSON.stringify(data))}`;
+};
+
+const sendBeacon = (url: string, data: any) => {
+  if (!url || !data) return;
+
+  const headers = {
+    type: 'application/x-www-form-urlencoded'
+  };
+
+  navigator.sendBeacon(url, new Blob([JSON.stringify(data)], headers));
+};
+
+const sendAjax = (url: string, data: any) => {
+  const client = new XMLHttpRequest();
+  client.open('POST', url, false);
+  client.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+  client.send(JSON.stringify(data));
+};
+
+const upload = (url: string, data: any) => {
+  const len = `${url}${url.indexOf('?') < 0 ? '?' : '&'}${encodeURIComponent(JSON.stringify(data))}`.length;
+
+  if (len < 2083) {
+    imgRequest(url, data);
+  } else if (!!navigator.sendBeacon) {
+    sendBeacon(url, data);
+  } else {
+    sendAjax(url, data);
+  }
+};
+
 // TODO
 export const createUploader = (config?: any): UploadHandler => {
   return data => {};
