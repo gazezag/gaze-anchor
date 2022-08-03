15a3950085d7da89d39e2d4fc9b238ab03333ff8
Ethan Teng <1345118870@qq.com>
2022-08-03 10:11:07 +0800

    added a main entry

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,26 @@
-export const add = (x: number, y: number): number => {
-  return x + y;
+import { WebPerformanceObserver } from 'core/webPerformance';
+import { GazeConfig } from 'types/gaze';
+import { get, getKeys, has, set } from 'utils/reflect';
+
+const mergeConfig = (userConfig: GazeConfig): GazeConfig => {
+  return getKeys(userConfig).reduce(
+    (prev, k) => {
+      has(prev, k) && set(prev, k, get(userConfig, k));
+      return prev;
+    },
+    {
+      target: 'http://localhost:9000',
+      logErrors: false,
+      release: ''
+    }
+  );
 };
+
+export class Gaze {
+  static init(config: GazeConfig) {
+    const mergedConfig = mergeConfig(config);
+
+    new WebPerformanceObserver(mergedConfig).init();
+    // TODO
+  }
+}
