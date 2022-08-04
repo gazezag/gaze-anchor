c9c6c3bf107895d5aa5e57e799d0a60159b8e137
Ethan Teng <1345118870@qq.com>
2022-08-04 19:38:29 +0800

    added isBoolean function

--- packages/utils/typeJudgment.ts
+++ packages/utils/typeJudgment.ts
@@ -3,3 +3,5 @@ export const isNumber = (data: any) => typeof data === 'number';
 export const isString = (data: any) => typeof data === 'string';
 
 export const isArray = (data: any) => Array.isArray(data);
+
+export const isBoolean = (data: any) => typeof data === 'boolean';
