806c403b481b9be5a1baa922d81a93f560fec417
Ethan Teng <1345118870@qq.com>
2022-09-25 00:01:02 +0800

    added release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,17 +1,40 @@
 const { resolve } = require('path');
-const { baseRunner, getArgs } = require('./utils');
+const { baseRunner, npmRunner, gitRunner, getArgs } = require('./utils');
 const packageJSON = require('../package.json');
 
 const rootPath = resolve(__dirname, '..');
-const distPath = resolve(__dirname, '../dist');
 
 const moveFiles = async () => {
   await baseRunner('cp', ['package.json', 'README.md', 'README-zh.md', 'dist'], rootPath);
 };
 
 const getUpdatedVersion = () => {
-  const [mainVersion, subVersion, phaseVersion] = packageJSON.version.split('.');
-  return `${mainVersion}.${subVersion}.${+phaseVersion + 1}`;
+  // const [mainVersion, subVersion, phaseVersion] = packageJSON.version.split('.');
+  // return `${mainVersion}.${subVersion}.${+phaseVersion + 1}`;
+  return packageJSON.version;
 };
 
-const publish = async () => {};
+const updateVersion = async () => {
+  const [kind] = getArgs();
+  await npmRunner('version', kind.slice(2), rootPath);
+};
+
+const commit = async () => {
+  await gitRunner('add', '-a');
+  await gitRunner('commit', ['-m', `updated version: ${getUpdatedVersion()}`]);
+  await gitRunner('push', ['github', 'master']);
+};
+
+const publish = async () => {
+  await baseRunner('cd', 'dist');
+  await npmRunner('run', 'publish');
+};
+
+const release = async () => {
+  await moveFiles();
+  await updateVersion();
+  await commit();
+  await publish();
+};
+
+release();
