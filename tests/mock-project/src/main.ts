7f78f033dac71d95fb3e855e56cc65c322d19d0f
Ethan Teng <1345118870@qq.com>
2022-09-25 01:54:37 +0800

    refactored construction system base on rollup

--- tests/mock-project/src/main.ts
+++ tests/mock-project/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+import { createGaze } from 'gaze-anchor';
+import {} from 'gaze-anchor/plugins';
 
 createApp(App).mount('#app');
